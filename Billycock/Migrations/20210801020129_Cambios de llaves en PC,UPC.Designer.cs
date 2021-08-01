8d351a650cc6a85209e9bcc380fa0dd06fad69ff
Victor <vlarosa91292@outlook.com>
2021-07-31 23:02:51 -0500

    Cambios de llaves PC,UPC

--- Billycock/Migrations/20210801020129_Cambios de llaves en PC,UPC.Designer.cs
+++ Billycock/Migrations/20210801020129_Cambios de llaves en PC,UPC.Designer.cs
@@ -4,14 +4,16 @@ using Billycock.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Billycock.Migrations
 {
     [DbContext(typeof(BillycockServiceContext))]
-    partial class BillycockServiceContextModelSnapshot : ModelSnapshot
+    [Migration("20210801020129_Cambios de llaves en PC,UPC")]
+    partial class CambiosdellavesenPCUPC
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
